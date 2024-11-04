de3d18c13ac3acf5d7b53772a674df914428a126
Quentin Dusserre <quentin.dusserre@ekwateur.fr>
2024-11-04 18:02:16 +0100

    call proxy 5mn

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const request = require('request');
 const cors = require('cors');
 const app = express();
+const axios = require('axios');
 
 // Activer CORS pour toutes les requêtes
 app.use(cors());
@@ -12,15 +13,20 @@ app.get('/apimeteo', (req, res) => {
     const username = process.env.USERNAME_METEOMATICS;
     const password = process.env.PASSWORD_METEOMATICS;
     const encodedCredentials = Buffer.from(`${username}:${password}`).toString('base64');
+
     if (!targetUrl) {
         return res.status(400).send("URL cible manquante dans la requête.");
     }
+
     request({
         url: targetUrl,
         headers: {
             'Authorization': 'Basic ' + encodedCredentials,
             'Content-Type': 'application/json'
         }
+    }).on('error', (err) => {
+        console.error('Erreur lors de la requête :', err);
+        res.status(500).send('Erreur lors de l\'appel de l\'API cible.');
     }).pipe(res);
 });
 
@@ -28,16 +34,34 @@ app.get('/meteofrance', (req, res) => {
     console.log("Call /meteofrance");
     const targetUrl = req.query.url;
     const apiKey = process.env.API_KEY_METEOFRANCE;
+
     if (!targetUrl) {
         return res.status(400).send("URL cible manquante dans la requête.");
     }
+
     request({
         url: targetUrl,
         headers: {
             'apikey': apiKey
         }
+    }).on('error', (err) => {
+        console.error('Erreur lors de la requête :', err);
+        res.status(500).send('Erreur lors de l\'appel de l\'API cible.');
     }).pipe(res);
 });
 
+// Fonction pour appeler le proxy
+async function callProxy() {
+    console.log("Call proxy");
+    try {
+        const response = await axios.get('https://votre-proxy.render.com/endpoint');
+    } catch (error) {
+        console.error('Erreur lors de l\'appel du proxy :', error);
+    }
+}
+
+// Appeler la fonction callProxy toutes les 5 minutes (300000 ms)
+setInterval(callProxy, 300000);
+
 // Lancer le serveur sur le port par défaut de Render (ou 3000 en local)
-app.listen(process.env.PORT || 3000, () => console.log("Proxy CORS en ligne !"));
+app.listen(process.env.PORT, () => console.log("Proxy CORS en ligne !"));
